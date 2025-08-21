58fd310bf93cfc72b697e96d0d7d1ef37f8a8f4b
jang <woojay09@gmail.com>
2025-08-22 07:59:37 +0900

    ch15 ex12 (Web Crawler): crawler(ch7) + indexer; load a page, index the page, find and add all the new links on the page to the queue (BFS)

--- code/src/com/allendowney/thinkdast/WikiCrawler.java
+++ code/src/com/allendowney/thinkdast/WikiCrawler.java
@@ -56,7 +56,33 @@ public class WikiCrawler {
 	 */
 	public String crawl(boolean testing) throws IOException {
 		// TODO: FILL THIS IN!
-        return null;
+		if (queue.isEmpty()) {
+			return null;
+		}
+		// 1. remove a URL from the queue
+		String url = queue.poll();
+
+		// unless it's in testing mode: if URL already indexed, should not index it again, return null
+		if (!testing && index.isIndexed(url)) {
+			System.out.println("Already indexed.");
+			return null;
+		}
+
+		Elements paragraphs;
+		if (testing) {
+			// 2. read the contents of the cached copies of the pages under /resource
+			paragraphs = wf.readWikipedia(url);
+		} else {
+			// otherwise, fetch current content from the web
+			paragraphs = wf.fetchWikipedia(url);
+		}
+		// 3. index pages (regardless of whether they are already indexed; when testing is true)
+		index.indexPage(url, paragraphs);
+		// 4. find all the internal links on the page, and add them to the queue in order
+		queueInternalLinks(paragraphs);
+		// printQueue();
+		// 5. return the URL of the page it indexed
+		return url;
 	}
 
 	/**
@@ -67,6 +93,28 @@ public class WikiCrawler {
 	// NOTE: absence of access level modifier means package-level
 	void queueInternalLinks(Elements paragraphs) {
         // TODO: FILL THIS IN!
+		// find all internal links for each paragraph
+		for (Element paragraph : paragraphs) {
+			queueInternalLinks(paragraph);
+		}
+	}
+
+	private void queueInternalLinks(Element paragraph) {
+		Elements elts = paragraph.select("a[href]");
+		for (Element elt : elts) {
+			String relURL = elt.attr("href");
+			if (relURL.startsWith("/wiki/")) {
+				// System.out.println("text: " + elt.text());
+				// System.out.println("href: " + href);
+				String absURL = elt.attr("abs:href");
+				queue.offer(absURL);
+			}
+		}
+	}
+
+	public void printQueue() {
+		System.out.println(queue);
+		System.out.println(queueSize());
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -77,8 +125,9 @@ public class WikiCrawler {
 		WikiCrawler wc = new WikiCrawler(source, index);
 		
 		// for testing purposes, load up the queue
-		Elements paragraphs = wf.fetchWikipedia(source);
-		wc.queueInternalLinks(paragraphs);
+		// Elements paragraphs = wf.fetchWikipedia(source);
+		// wc.queueInternalLinks(paragraphs);
+		// wc.printQueue();
 
 		// loop until we index a new page
 		String res;
@@ -86,8 +135,8 @@ public class WikiCrawler {
 			res = wc.crawl(false);
 
             // REMOVE THIS BREAK STATEMENT WHEN crawl() IS WORKING
-            break;
-		} while (res == null);
+            // break;
+		} while (res == null); // ends when you indexed a new page (so it ends when you found the first new link)
 		
 		Map<String, Integer> map = index.getCounts("the");
 		for (Entry<String, Integer> entry: map.entrySet()) {
